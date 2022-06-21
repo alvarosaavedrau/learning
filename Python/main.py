7639a70cf47e9885110d6d293b3f06b5f9d2cd3a
alvarosaavedrau <alvarosaavedrau@gmail.com>
2022-06-21 16:44:20 +0200

    Learning Python started

---
+++ Python/main.py
@@ -0,0 +1,10 @@
+# Printear algo por pantalla
+print("hola mundo")
+
+# Imprimir comillas en un print (escapadas \)
+print("Hola mundo \"comillas\"")
+
+# Para imprimir números no hace falta utilizar las comillas ""
+print(100)
+
+print(100 + 50)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# learning
+# Instalación Python
+## Windows
+[Enlace descarga](https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe)

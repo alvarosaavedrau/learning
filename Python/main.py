e58a314125c80293d70fffacc7c457e90807e2ee
alvarosaavedrau <alvarosaavedrau@gmail.com>
2022-06-24 13:48:15 +0200

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,4 @@
-# Printear algo por pantalla
+# Imprimir algo por pantalla
 print("hola mundo")
 
 # Imprimir comillas en un print (escapadas \)
@@ -7,4 +7,8 @@ print("Hola mundo \"comillas\"")
 # Para imprimir números no hace falta utilizar las comillas ""
 print(100)
 
+# Suma de números enteros
 print(100 + 50)
+
+# Encadenación de caracteres
+print("hola " + "mundo")
